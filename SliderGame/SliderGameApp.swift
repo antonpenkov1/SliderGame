4494a0dd49e011f5feeb3004edfaff1adea31b0b
anton_penkov <anton.penkov1@gmail.com>
2024-03-12 16:04:31 +0100

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Антон Пеньков on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
